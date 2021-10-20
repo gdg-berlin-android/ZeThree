cfeae234df09c0520d4c7b34fe7d2a3200370d1e
Mario Bodemann <mario.bodemann@gmail.com>
2021-10-20 14:45:56 +0200

    mohsenoid

---
+++ app/src/main/java/berlindroid/zethree/MainActivity.kt
@@ -0,0 +1,125 @@
+package berlindroid.zethree
+
+import android.app.Activity
+import android.os.Bundle
+import berlindroid.zethree.R
+import android.view.View.OnLongClickListener
+import android.content.Intent
+import android.view.View
+import berlindroid.zethree.cats.CatsActivity
+import berlindroid.zethree.dogs.DogsActivity
+import android.view.ViewGroup
+import android.widget.TextView
+import android.view.animation.BounceInterpolator
+import android.widget.Toast
+import androidx.compose.foundation.ExperimentalFoundationApi
+import berlindroid.zethree.UpdateActivity
+import androidx.work.PeriodicWorkRequest
+import berlindroid.zethree.util.UpdateHandlerControllerManagerRefresher
+import androidx.work.BackoffPolicy
+import androidx.work.WorkManager
+import androidx.work.ExistingPeriodicWorkPolicy
+import java.time.Duration
+import java.util.*
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        scheduleUpdates()
+    }
+
+    public override fun onResume() {
+        findViewById<View>(R.id.main_cat_button).setOnLongClickListener { view: View? ->
+            onCatsLongClicked(
+                findViewById(R.id.main_cat_button)
+            )
+            true
+        }
+        super.onResume()
+    }
+
+    @OptIn(ExperimentalFoundationApi::class)
+    fun onCatsClicked(view: View?) {
+        startActivity(Intent(this, CatsActivity::class.java))
+    }
+
+    fun onDogsClicked(view: View?) {
+        startActivity(Intent(this, DogsActivity::class.java))
+    }
+
+    fun onCatsLongClicked(view: View?) {
+        val cats: MutableList<String> = ArrayList()
+        cats.add("😻")
+        cats.add("😺")
+        cats.add("😸")
+        cats.add("😹")
+        cats.add("😼")
+        cats.add("😽")
+        cats.add("🙀")
+        cats.add("😿")
+        cats.add("😾")
+        cats.add("🐱")
+        cats.add("🐈")
+        val r = Random()
+        val width = 1000
+        val height = 2000
+        val duration = 5000
+        val root = findViewById<ViewGroup>(R.id.main_root)
+        for (i in 0..49) {
+            val cat = TextView(this)
+            cat.text = cats[r.nextInt(cats.size)]
+            cat.x = r.nextFloat() * width
+            cat.y = 0f
+            cat.alpha = 0.0f
+            cat.rotation = r.nextFloat() * 360.0f
+            cat.animate().translationY(height * 1.0f).setDuration(duration.toLong()).rotation(360f)
+                .setStartDelay(r.nextInt(duration / 2).toLong())
+                .withEndAction { root.removeView(cat) }
+                .withStartAction { cat.alpha = 1.0f }.setInterpolator(BounceInterpolator()).start()
+            root.addView(cat, 80, 80)
+        }
+    }
+
+    fun tryMeFun(view: View?) {
+        Toast.makeText(
+            this,
+            "Hey, it's fun and built upon me",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    fun doYourThingClicked(view: View?) {
+        Toast.makeText(
+            this,
+            """
+                Hey, do what ever you want, you got 15 minutes!
+                
+                No idea what to do? Look around you for some task  you!
+                """.trimIndent(),
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    fun checkForUpdates(view: View?) {
+        val i = Intent(this, UpdateActivity::class.java)
+        startActivity(i)
+    }
+
+    fun scheduleUpdates() {
+        val findNewApkRequest = PeriodicWorkRequest.Builder(
+            UpdateHandlerControllerManagerRefresher::class.java,
+            Duration.ofMinutes(60),
+            Duration.ofMinutes(15)
+        )
+            .setBackoffCriteria(BackoffPolicy.LINEAR, Duration.ofMinutes(20))
+            .build()
+        WorkManager
+            .getInstance(this)
+            .enqueueUniquePeriodicWork(
+                "app_updater_background",
+                ExistingPeriodicWorkPolicy.KEEP,
+                findNewApkRequest
+            )
+    }
+}
---
+++ app/src/main/java/berlindroid/zethree/UpdateActivity.kt
@@ -0,0 +1,74 @@
+package berlindroid.zethree
+
+import android.app.Activity
+import android.os.Bundle
+import berlindroid.zethree.R
+import android.annotation.SuppressLint
+import android.os.AsyncTask
+import android.os.Looper
+import berlindroid.zethree.util.LowQualityUpdateDownloader.ApkResolution
+import berlindroid.zethree.util.LowQualityUpdateDownloader
+import android.widget.Toast
+import android.content.Intent
+import android.net.Uri
+import android.os.Handler
+import android.view.View
+
+class UpdateActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.update_activity)
+    }
+
+    // risky only if you want it to be risky !!!!!!111one
+    @SuppressLint("StaticFieldLeak")
+    override fun onStart() {
+        super.onStart()
+
+        // AsyncTask is the new Coroutine Job!
+        object : AsyncTask<Void?, Void?, Void?>() {
+            override fun doInBackground(vararg params: Void?): Void? {
+                // cause some fake delay to prevent GitHub's rate-limiter from kicking in
+                try {
+                    Thread.sleep(1000)
+                } catch (ignored: InterruptedException) {
+                }
+                Handler(Looper.getMainLooper()).post {
+                    findViewById<View>(R.id.updateWaitText).visibility = View.VISIBLE
+                }
+                return null
+            }
+        }.execute()
+        object : AsyncTask<Void?, Void?, Void?>() {
+            override fun doInBackground(vararg params: Void?): Void? {
+                val result = LowQualityUpdateDownloader.resolveLatestApk()
+                Handler(Looper.getMainLooper()).post {
+                    when (result.status) {
+                        LowQualityUpdateDownloader.Status.FAILED -> Toast.makeText(
+                            this@UpdateActivity,
+                            "Can't fetch the update: " + result.errorMessage,
+                            Toast.LENGTH_LONG
+                        ).show()
+                        LowQualityUpdateDownloader.Status.APK_AVAILABLE -> {
+                            Toast.makeText(
+                                this@UpdateActivity,
+                                "Next: Install the downloaded file",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            val i = Intent(Intent.ACTION_VIEW)
+                            i.data = Uri.parse(result.url)
+                            startActivity(i)
+                        }
+                        LowQualityUpdateDownloader.Status.ALREADY_LATEST -> Toast.makeText(
+                            this@UpdateActivity,
+                            "Already at the latest version: v" + result.appVersion,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    finish()
+                }
+                return null
+            }
+        }.execute()
+    }
+}
